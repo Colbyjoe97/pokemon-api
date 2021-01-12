a51e6f1ee152dbe74e4bb20e6e8e6297e9c4fea8
Colbyjoe97 <dumonchelle123@gmail.com>
2021-01-12 16:38:41 -0500

    first commit

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,59 +1,58 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Pokemon = () => {
 
-    const [poke, setPoke] = useState([])
+    const [pokeData, setPokeData] = useState([])
 
     // Shows API data without needing to click the button
-    // useEffect(()=>{
-    //     fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=807")
-    //         .then(pokemonData => {
-    //             return pokemonData.json()
-    //         })
-    //         .then(pokemonData => {
-    //             console.log(pokemonData.results)
-    //             setPoke(pokemonData.results)
-    //         })
-    //         .catch(err => {
-    //             console.log(err)
-    //         })
-    // }, [])
-
-    const getPokemon = (e) => {
-        fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=807")
+    useEffect(()=>{
+        axios.get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=807")
             .then(pokemonData => {
-                return pokemonData.json()
+                console.log("*************")
+                console.log(pokemonData.data.results)
+                console.log("*************")
+                setPokeData(pokemonData.data.results)
             })
+            .catch(err => {
+                console.log(err)
+            })
+    }, []);
+
+    // Shows API data after clicking button (if useEffect isn't commented out, you can only see this in the console)
+    const getPokemon = (e) => {
+        axios.get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=807")
             .then(pokemonData => {
-                console.log(pokemonData.results)
-                setPoke(pokemonData.results)
+                console.log("*************")
+                console.log(pokemonData.data.results)
+                console.log("*************")
+                setPokeData(pokemonData.data.results)
             })
             .catch(err => {
                 console.log(err)
             })
     }
 
-
     return (
         <div>
             <h1>Pokemon To Catch</h1>
-            <button onClick={getPokemon}>Get Pokemon Names</button>
+            <button onClick={getPokemon} className="btn btn-success">Get Pokemon Names</button>
             {
                 <table className="table table-hover mt-3">
                     <thead>
                         <tr>
-                            <th>Pokemon ID</th>
+                            <th>Pokemon Id</th>
                             <th>Name</th>
                             <th>URL</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
-                            poke.map((pokeObj, i) => {
+                            pokeData.map((pokeObj, i) => {
                                 return <tr key={i}>
                                         <td>{i + 1}</td>
                                         <td>{pokeObj.name}</td>
-                                        <td>{pokeObj.url}</td>
+                                        <td><a href={pokeObj.url}>{pokeObj.url}</a></td>
                                     </tr>
                             })
                         }
